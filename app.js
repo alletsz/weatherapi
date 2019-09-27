aeaa16a999b171bf1c4dee5f80b4f2be8129a443
Stella Zhang <stellazhang@stella-zhangs-macbook-2.local>
2019-09-27 15:40:12 -0700

    Add additional api routes

--- app.js
+++ app.js
@@ -9,40 +9,50 @@ const bodyParser = require('body-parser')
 const api_key = process.env.API_KEY
 
 app.use(bodyParser.json())
-//get with exclusions
-// app.get('/', (req, res) => {
-//   fetch(`https://api.darksky.net/forecast/${api_key}/42.3601,-71.0589/?exclude=minutely,daily,alerts,flags`)
-//     .then(response => response.json()) //promise results to an object
-//     .then(resObj => {
-//       res.send(resObj)
-//     })
-// });
 
-app.get('/', (req, res) => {
+// get all
+app.get('/forecast', (req, res) => {
   fetch(`https://api.darksky.net/forecast/${api_key}/42.3601,-71.0589`)
     .then(response => response.json()) //promise results to an object
     .then(resObj => {
-      res.send(resObj.hourly)
+      res.send(resObj)
     })
 });
 
-/*
-grab latitude and longitude values from client
-comes in as an object {latlon: xxx, xxx}
-convert to array and split values [xxx, xxx]
-*/
+// get with exclusions
+app.get('/hourly', (req, res) => {
+  fetch(`https://api.darksky.net/forecast/${api_key}/42.3601,-71.0589/?exclude=minutely,daily,alerts,flags`)
+    .then(response => response.json()) //promise results to an object
+    .then(resObj => {
+      res.send(resObj)
+    })
+});
 
-// app.post('/:latlon', (req, res) => {
-//   const latlon = req.params.latlon.split(',')
-//   const lat = req.params[0]
-//   const lon = req.params[1]
+// get and return date and hourly weather info
+app.get('/day', (req, res) => {
+  fetch('https://api.darksky.net/forecast/390b0c743dc9817aa92fc025e346335f/42.3601,-71.0589/?exclude=minutely,daily,alerts,flags')
+    .then(response => response.json()) //promise results to an object
+    .then(resObj => {
+      const hoursArray = resObj.hourly.data
+      let timeVal = hoursArray.map((time) => {
+        return new Date(time.time * 1000).toISOString()
+      })
 
-// })
+      let weather = hoursArray.map((weather) => {
+        return weather.summary
+      })
 
-/*
-comes in as object {lat: xxx, lon: xxx}
-*/
+      let data = {
+        hours: timeVal,
+        summary: weather
+      }
 
+      res.send({ hour: timeVal, weather: weather })
+    })
+});
+
+
+// get info based on latitude and longitude provided by user
 app.post('/:lat/:lon', (req, res) => {
 
   const lat = req.params.lat
